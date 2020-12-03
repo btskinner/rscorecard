a676f0f1eeb049741be4b7bec6a59c2050aabb7b
btskinner <btskinner@coe.ufl.edu>
2020-12-03 17:29:37 -0500

    update for 2 December 2020 data (v0.17.0)

--- data-raw/make_dict_hash.R
+++ data-raw/make_dict_hash.R
@@ -1,16 +1,16 @@
 ## make_dict_hash.R
 
-libs <- c('dplyr','readxl','zoo')
+libs <- c("dplyr","readxl","zoo")
 sapply(libs, require, character.only = TRUE)
 
 ## get latest XLSX data dictionary file
-file <- 'CollegeScorecardDataDictionary.xlsx'
-link <- paste0('https://collegescorecard.ed.gov/assets/', file)
+file <- "CollegeScorecardDataDictionary.xlsx"
+link <- paste0("https://collegescorecard.ed.gov/assets/", file)
 download.file(link, file)
 
 ## sheet names
-sheets <- c('institution_data_dictionary',
-            'FieldOfStudy_data_dictionary')
+sheets <- c("institution_data_dictionary",
+            "FieldOfStudy_data_dictionary")
 
 ## read in each sheet, munge, and bind
 df <- purrr::map(sheets,
@@ -29,9 +29,9 @@ df <- purrr::map(sheets,
                      ## lower name values for varname column
                      mutate(varname = tolower(varname)) %>%
                      ## remove extra \r\n from description column
-                     mutate(description = gsub('^(.+)\r\n 0.*$', '\\1', description)) %>%
+                     mutate(description = gsub("^(.+)\r\n 0.*$", "\\1", description)) %>%
                      ## remove trailing characters from dev_friendly_name
-                     mutate(dev_friendly_name = gsub('^(.+):$', '\\1',
+                     mutate(dev_friendly_name = gsub("^(.+):$", "\\1",
                                                      dev_friendly_name)) %>%
                      ## roll values forward to fill NA
                      mutate(description = na.locf(description),
@@ -59,11 +59,11 @@ tmp <- df %>% distinct(varname, .keep_all = TRUE)
 
 for (i in 1:nrow(tmp)) {
     ## key/value pair (1)
-    key <- tmp[['varname']][i]
-    val <- tmp[['dev_friendly_name']][i]
+    key <- tmp[["varname"]][i]
+    val <- tmp[["dev_friendly_name"]][i]
     ## key/value pair (2)
-    key_c <- paste0(key, '_c')
-    val_c <- tmp[['dev_category']][i]
+    key_c <- paste0(key, "_c")
+    val_c <- tmp[["dev_category"]][i]
     ## assign
     sc_hash[[key]] <- val
     sc_hash[[key_c]] <- val_c
@@ -76,11 +76,11 @@ tmp <- df %>% distinct(dev_friendly_name, .keep_all = TRUE)
 
 for (i in 1:nrow(tmp)) {
     ## key/value pair (3)
-    key <- tmp[['dev_friendly_name']][i]
-    val <- tmp[['varname']][i]
+    key <- tmp[["dev_friendly_name"]][i]
+    val <- tmp[["varname"]][i]
     ## key/value pair (4)
-    key_c <- paste0(key, '_c')
-    val_c <- tmp[['dev_category']][i]
+    key_c <- paste0(key, "_c")
+    val_c <- tmp[["dev_category"]][i]
     ## assign
     sc_hash[[key]] <- val
     sc_hash[[key_c]] <- val_c
@@ -94,7 +94,7 @@ for (i in 1:nrow(tmp)) {
 
 ## users can specifiy the data source for these variables; by default,
 ## the information will continue to come from the institution data
-both_sources <- c('unitid', 'opeid6', 'control', 'main', 'instnm')
+both_sources <- c("unitid", "opeid6", "control", "main", "instnm")
 
 tmp <- df %>%
     filter(varname %in% both_sources) %>%
@@ -102,18 +102,18 @@ tmp <- df %>%
 
 for (i in 1:nrow(tmp)) {
     ## category == programs ? _f : _i
-    end <- ifelse(tmp[['dev_category']][i] == 'programs', '_f', '_i')
+    end <- ifelse(tmp[["dev_category"]][i] == "programs", "_f", "_i")
     ## key/value pair (with endings on key)
-    key <- paste0(tmp[['varname']][i], end)
-    val <- tmp[['dev_friendly_name']][i]
+    key <- paste0(tmp[["varname"]][i], end)
+    val <- tmp[["dev_friendly_name"]][i]
     ## key/value pair for category (with endings on key)
-    key_c <- paste0(key, '_c')
-    val_c <- tmp[['dev_category']][i]
+    key_c <- paste0(key, "_c")
+    val_c <- tmp[["dev_category"]][i]
     ## assign
     sc_hash[[key]] <- val
     sc_hash[[key_c]] <- val_c
 }
 
 ## save
-usethis::proj_set('..')
+usethis::proj_set("..")
 usethis::use_data(dict, sc_hash, overwrite = TRUE, internal = TRUE)
